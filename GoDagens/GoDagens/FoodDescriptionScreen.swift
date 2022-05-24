1e401887371bc4ed82d1f2e83eaa472417dce84e
Sebastian Warvsten <sebastian.warvsten@gmail.com>
2022-05-24 16:41:36 +0200

    Remaining fields added to FoodDescriptionScreen

--- GoDagens/GoDagens/FoodDescriptionScreen.swift
+++ GoDagens/GoDagens/FoodDescriptionScreen.swift
@@ -6,12 +6,11 @@
 //
 
 import Foundation
-
 import UIKit
 
 class FoodDescriptionScreen: UIViewController {
     
-    private let textView: UITextView = {
+    private let nameTextView: UITextView = {
         let textView = UITextView()
         textView.text = "Maträttens namn"
         textView.textColor = .black
@@ -27,6 +26,33 @@ class FoodDescriptionScreen: UIViewController {
         imageView.backgroundColor = .black
         return imageView
     }()
+    
+    private let timeTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "5"
+        textView.textColor = .black
+        textView.font = UIFont(name: textView.font!.fontName, size: 18)
+        textView.isEditable = false
+        return textView
+    }()
+    
+    private let descriptionTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Beskrivning om maten"
+        textView.textColor = .black
+        textView.font = UIFont(name: textView.font!.fontName, size: 18)
+        textView.isEditable = false
+        return textView
+    }()
+    
+    private let ingredientsTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Beskrivning om maten"
+        textView.textColor = .black
+        textView.font = UIFont(name: textView.font!.fontName, size: 18)
+        textView.isEditable = false
+        return textView
+    }()
 
     var matArray = [FoodModel]()
     var selectedFood: Int = 0
@@ -34,7 +60,10 @@ class FoodDescriptionScreen: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.addSubview(textView)
+        view.addSubview(nameTextView)
+        view.addSubview(descriptionTextView)
+        view.addSubview(timeTextView)
+        view.addSubview(ingredientsTextView)
         
         view.addSubview(imageView)
         imageView.frame = CGRect(x: (view.frame.width-300)/2, y: view.safeAreaInsets.top+200, width: 300, height: 300)
@@ -44,16 +73,22 @@ class FoodDescriptionScreen: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        textView.frame = CGRect(x: 50, y: 50, width: 300, height: 200)
-        textView.centerVertically()
+        nameTextView.frame = CGRect(x: 50, y: 50, width: 300, height: 200)
+        nameTextView.centerVertically()
+        descriptionTextView.frame = CGRect(x: 50, y: 550, width: 300, height: 200)
+        timeTextView.frame = CGRect(x: 50, y: 500, width: 100, height: 50)
+        ingredientsTextView.frame = CGRect(x: 50, y: 600, width: 200, height: 200)
     }
     
     @objc func getRandomFood() {
-        self.textView.text = self.matArray[self.selectedFood].name
+        self.nameTextView.text = self.matArray[self.selectedFood].name
         let url = URL(string: self.matArray[self.selectedFood].imageURL)!
         guard let data = try? Data(contentsOf: url) else {
             return
         }
         self.imageView.image = UIImage(data: data)
+        self.descriptionTextView.text = self.matArray[self.selectedFood].descrip
+        self.timeTextView.text = String(self.matArray[self.selectedFood].time).appending(" min")
+        self.ingredientsTextView.text = self.matArray[self.selectedFood].ingrediets.joined(separator: "\n")
     }
 }
